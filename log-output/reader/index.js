007ba9d0a5a5bb5fcb1485c33144fb1154d41b76
dev1abhi <abhilashsarangi222@gmail.com>
2025-08-14 22:21:52 +0530

    configmap implementation in log-output

--- log-output/reader/index.js
+++ log-output/reader/index.js
@@ -6,21 +6,21 @@ const { v4: uuidv4 } = require('uuid');
 
 const PORT = process.env.PORT || 3000;
 const PINGPONG_URL = 'http://pingpong-svc:1235/pings';
+const MESSAGE = process.env.MESSAGE || 'No message set';
+const INFO_FILE_PATH = '/etc/config/information.txt'; 
 
 
-// const logPath = '/shared/log.txt'; //from non-persistent volume
+// const logPath = '/shared/log.txt'; //from non-persistent volume (between log reader and log writer)
 // const countPath = '/data/count.txt'; // from persistent volume (shared between pingpong app and log output)
 
 // app.get('/status', (req, res) => {
 //   const logContent = fs.existsSync(logPath) ? fs.readFileSync(logPath, 'utf8') : 'No logs yet.';
 //   const count = fs.existsSync(countPath) ? fs.readFileSync(countPath, 'utf8') : 'unknown';
-
 //   res.send(`<pre>${logContent.trim()} \nPing / Pongs: ${count}</pre>`);
 // });
 
 
 app.get('/', (req, res) => {
-    //req given inside cluster
   http.get(PINGPONG_URL, (pingRes) => {
     let data = '';
     pingRes.on('data', chunk => data += chunk);
@@ -28,12 +28,21 @@ app.get('/', (req, res) => {
       const timestamp = new Date().toISOString();
       const uuid = uuidv4();
 
-      // Clean and extract number from pingpong response
+      // Extract number from pingpong response
       const count = data.match(/\d+/)?.[0] || '0';
 
-      const log = `${timestamp}: ${uuid}. Ping / Pongs: ${count}`;
-      console.log(log);
-      res.send(log);
+      // Read file content from ConfigMap
+      let fileContent = '';
+      try {
+        fileContent = fs.readFileSync(INFO_FILE_PATH, 'utf-8').trim();
+      } catch (err) {
+        console.error('Error reading file from ConfigMap:', err.message);
+        fileContent = '(file not found)';
+      }
+
+      const log = `${fileContent}\n${MESSAGE}\n${timestamp}: ${uuid}.\nPing / Pongs: ${count}`;
+      //console.log(log.split("\n"));
+      res.send(`<pre>${log}</pre>`);
     });
   }).on('error', (err) => {
     console.error('Pingpong service error:', err.message);
--- log-output/writer/index.js
+++ log-output/writer/index.js
@@ -4,6 +4,7 @@ const crypto = require('crypto');
 
 const id = crypto.randomUUID();
 
+//saving in shared/log.txt
 setInterval(() => {
   const timestamp = new Date().toISOString();
   const line = `${timestamp}: ${id}\n`;
